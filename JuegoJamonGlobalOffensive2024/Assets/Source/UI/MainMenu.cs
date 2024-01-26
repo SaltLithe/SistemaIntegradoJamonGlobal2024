59efd4ea68c4b677ac9f572aaec106e653ab7f4b
Juan <9juanbc9@gmail.com>
2024-01-26 20:08:02 +0100

    Main Menu WIP

---
+++ JuegoJamonGlobalOffensive2024/Assets/Source/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string _GameSceneName;
+    [SerializeField] private Button _ButtonPlayGame;
+
+    private void Awake()
+    {
+        _ButtonPlayGame.onClick.RemoveAllListeners();
+        _ButtonPlayGame.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(_GameSceneName);
+    }
+}
