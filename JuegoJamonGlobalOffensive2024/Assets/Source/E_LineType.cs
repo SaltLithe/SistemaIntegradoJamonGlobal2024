d65c26ccb687b0849f522d54517b0c63e350a6c2
RanaMarrana <offcarmen@gmail.com>
2024-01-26 20:25:02 +0100

    line classes

---
+++ JuegoJamonGlobalOffensive2024/Assets/Source/E_LineType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_LineType
+{
+    RED,
+    GREEN,
+    BLUE,
+    PURPLE,
+    YELLOW,
+    BLACK,
+    NEUTRAL
+}
---
+++ JuegoJamonGlobalOffensive2024/Assets/Source/Line.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line 
+{
+    public E_LineType _type;
+    public int _baseEffect;
+    public float _time;
+
+}
