062bc0f2cecfa2500f1d500d0db1f489753b2871
Andrés Viñarta <andresvinarta@gmail.com>
2024-01-27 16:46:08 +0100

    GameManager v2 y FadeToBlack

---
+++ JuegoJamonGlobalOffensive2024/Assets/Source/UI/FadeToBlack.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeToBlack : MonoBehaviour
+{
+
+    private bool _fading;
+    private float _fadeDuration;
+    private int _fadeDirection;
+    private float _initTime;
+    private Color _color;
+
+    [SerializeField]
+    private Image _image;
+
+    private void Start()
+    {
+        _color = Color.black;
+        _image.color = _color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timePassed = Time.time - _initTime;
+        if (_fading)
+        {
+            _color.a += Time.deltaTime/_fadeDuration * _fadeDirection;
+            if(timePassed >= _fadeDuration)
+            {
+                _fading = false;
+                if(_fadeDirection == 1) 
+                { 
+                    _color.a = 1; 
+                }
+                else 
+                {  
+                    _color.a = 0; 
+                }
+                _fadeDirection = 0;
+            }
+        }
+    }
+
+    public void ActivateFade(bool fading, float duration, int direction)
+    {
+        this._fading = fading;
+        this._fadeDuration = duration;
+        this._fadeDirection = direction;
+        this._initTime = Time.time;
+    }
+}
