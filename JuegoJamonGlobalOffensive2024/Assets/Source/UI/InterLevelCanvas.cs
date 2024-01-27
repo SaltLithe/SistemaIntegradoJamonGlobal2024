f81f3c269195f21ccff0f99b35c725a809e1ae53
Andrés Viñarta <andresvinarta@gmail.com>
2024-01-27 19:22:33 +0100

    InterLevel hecho

--- JuegoJamonGlobalOffensive2024/Assets/Source/UI/InterLevelCanvas.cs
+++ JuegoJamonGlobalOffensive2024/Assets/Source/UI/InterLevelCanvas.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class InterLevelCanvas : MonoBehaviour
 {
+
+    [SerializeField]
+    private Canvas _fadeCanvas;
+
+    [SerializeField]
+    private float _duration;
+
+    [SerializeField]
+    private int _gameSceneIndex, _mainMenuIndex;
+
+    [SerializeField]
+    private Image _image;
+
+    [SerializeField]
+    private Sprite[] _sprites;
+
+    private static int _currentSprite = 0;
+
+    private bool _canLoad = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _image.sprite = _sprites[_currentSprite];
+        _currentSprite++;
+        _fadeCanvas.GetComponent<FadeToBlack>().ActivateFade(true, _duration, 0);
+        StartCoroutine(WaitCoroutine(_duration));
+        _canLoad = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator WaitCoroutine(float seconds)
     {
-        
+        yield return new WaitForSeconds(seconds);
+    }
+
+    public void NextButtonPressed()
+    {
+        if (_canLoad && _currentSprite < _sprites.Length - 1) 
+        {
+            SceneManager.LoadScene(_gameSceneIndex);
+        }
+        else if(_currentSprite >= _sprites.Length - 1)
+        {
+            SceneManager.LoadScene(_gameSceneIndex);
+        }
     }
 }
