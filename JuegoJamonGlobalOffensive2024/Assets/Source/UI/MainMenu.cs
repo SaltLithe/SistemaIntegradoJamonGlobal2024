c26426df3e8f753a2e79effe2a61ea331d2abc9b
Juan <9juanbc9@gmail.com>
2024-01-28 14:42:19 +0100

    asdf

--- JuegoJamonGlobalOffensive2024/Assets/Source/UI/MainMenu.cs
+++ JuegoJamonGlobalOffensive2024/Assets/Source/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,12 +7,20 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string _GameSceneName;
     [SerializeField] private Button _ButtonPlayGame;
+    [SerializeField] private Button _ButtonExitGame;
 
     private void Awake()
     {
         _ButtonPlayGame.onClick.RemoveAllListeners();
         _ButtonPlayGame.onClick.AddListener(StartGame);
+        _ButtonExitGame.onClick.AddListener(ExitGame);
     }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
     private void Start()
     {
         AudioManager.Instance.StartMenuMusic();
